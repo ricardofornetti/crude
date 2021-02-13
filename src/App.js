3cd5d23bc0ca973159c0eb934696d4da6d60fe1f
ricardofornetti <77638506+ricardofornetti@users.noreply.github.com>
2021-02-13 11:35:04 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container mt-10">
+      <h1>Tareas</h1> 
+      <hr/>
+    </div>
+   
+   )
+}
+
+export default App;
